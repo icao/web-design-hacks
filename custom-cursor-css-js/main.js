e2390211f1dd0597a86104d9b08eaaf138124934
César Aparicio <clark012389@hotmail.com>
2019-01-27 15:09:19 -0600

    Finalizando ejemplo de cursor customizado

--- custom-cursor-css-js/main.js
+++ custom-cursor-css-js/main.js
@@ -1,15 +1,14 @@
 (() => { 
-   console.log('%c Hola mundo...!!!', 'font-size: 20px; color:mediumspringgreen;');
 
    const cursor = document.querySelector('.cursor');
 
    document.addEventListener('mousemove', e => {
-      console.log(e);
       cursor.setAttribute('style', `top:  ${e.pageY - 25}px; left: ${e.pageX - 25}px;`);
    });
 
    document.addEventListener('click', () => { 
-      console.log('%c Click...!!!', 'font-size: 20px; color:orange;');
+      console.log("%c Click...!!!", "font-size: 20px; color:mediumspringgreen;");
+
       cursor.classList.add('cursor--expand');
       console.log(cursor.classList);
    
