551e304a8035a35334f062452c93c1061b5d0702
César Aparicio <clark012389@hotmail.com>
2019-08-18 05:25:42 -0500

    Regalo de aniversario de mi novia en proceso

---
+++ 00-aniversario-un-mes-pecosita/index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const pistaAudio = document.querySelector("#pistaAudio");
+  const slideActivo = document.querySelector("#slide__nueve");
+
+  window.addEventListener("scroll", function() {
+    // Medicion de el ScrollTop - Posicion respecto al Top de la pagina
+    let windowHeight =
+      (document.documentElement && document.documentElement.scrollTop) ||
+      document.body.scrollTop;
+    // Posicion inicial despecto al TOP, donde se ejecutara solo el evento
+    let inicioSlideActivo = slideActivo.offsetTop;
+    // Posicion final despecto al TOP, donde se ejecutara solo el evento
+    let finalSlideActivo = slideActivo.offsetTop + slideActivo.offsetHeight;
+
+    // Si se encuentra dentro del espacio de eventos, haz play a la pista
+    if (windowHeight >= inicioSlideActivo && windowHeight <= finalSlideActivo) {
+      pistaAudio.play();
+    }
+  });
+});
