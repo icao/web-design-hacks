0193ef85f7c4c9343387b1b94cc3eeb7d150355b
César Aparicio <clark012389@hotmail.com>
2021-02-28 17:41:34 -0600

    Refactorizacion de codigo de la segunda implementacion

--- hover-effect-cursor-position/index-dos.js
+++ hover-effect-cursor-position/index-dos.js
@@ -3,17 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   for (const item of colection) {
     let detail = item.querySelector(".detail");
+
     item.addEventListener("mouseenter", function (event) {
-      // console.log(event.target.clientHeight); //referencia a coleciton__item
-      // console.log(event.target.clientWidth); //referencia a coleciton__item
-      // console.log(this.clientHeight); //referencia a coleciton__item
-      // console.log(this.clientWidth); //referencia a coleciton__item
-
-      console.log("DETAIL", detail);
-
-      console.log(item.clientHeight); //referencia a coleciton__item
-      console.log(item.clientWidth); //referencia a coleciton__item
-
       if (!detail.clientWidth && !detail.clientHeight) {
         let maximumMeasure = Math.max(item.clientWidth, item.clientHeight);
         detail.setAttribute(
@@ -22,23 +13,18 @@ document.addEventListener("DOMContentLoaded", () => {
         );
       }
 
-      let x = event.pageX - event.target.offsetLeft - detail.clientWidth / 2;
-      let y = event.pageY - event.target.offsetTop - detail.clientHeight / 2;
+      let x = event.layerX - detail.clientWidth / 2;
+      let y = event.layerY - detail.clientHeight / 2;
 
       detail.style.setProperty("top", `${y}px`);
       detail.style.setProperty("left", `${x}px`);
 
       detail.classList.add("detail--animated");
     });
-  }
 
-  for (const item of colection) {
-    let detail = item.querySelector(".detail");
     item.addEventListener("mouseleave", function (event) {
-      // let detail = event.target.childNodes[3];
-      // let detail = item.querySelector(".detail");
-      let x = event.pageX - event.target.offsetLeft - detail.clientWidth / 2;
-      let y = event.pageY - event.target.offsetTop - detail.clientHeight / 2;
+      let x = event.layerX - detail.clientWidth / 2;
+      let y = event.layerY - detail.clientHeight / 2;
 
       detail.style.setProperty("top", `${y}px`);
       detail.style.setProperty("left", `${x}px`);
