42743085e001c3d958754f2b2504deb3dfd3d114
César Aparicio <clark012389@hotmail.com>
2021-02-28 18:58:32 -0600

    Refactor implementacion 1

--- hover-effect-cursor-position/index.js
+++ hover-effect-cursor-position/index.js
@@ -2,49 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const elements = document.querySelectorAll(".grid__item");
 
   for (const item of elements) {
-    // console.log("PROIPS: ", item);
-    // console.log("WIDTH: ", item.clientWidth);
-    // console.log("WIDTH: ", item.clientHeight);
-    item.addEventListener("mousemove", function (event) {
-      // console.log("EJE X: ", event.layerX);
-      // console.log("EJE Y: ", event.layerY);
+    item.addEventListener("mouseenter", function (event) {
+      let maximumMeasure = Math.max(item.clientWidth, item.clientHeight);
+      item.setAttribute("style", `--diameter: ${maximumMeasure}px;`);
 
-      let x = event.layerY;
-      let y = event.layerX;
+      let x = event.layerY - maximumMeasure / 2;
+      let y = event.layerX - maximumMeasure / 2;
 
-      item.style.setProperty("--top-before", `${x - 100}px`);
-      item.style.setProperty("--left-before", `${y - 100}px`);
+      item.style.setProperty("--position-top-before", `${x}px`);
+      item.style.setProperty("--position-left-before", `${y}px`);
     });
 
-    item.addEventListener("mouseLeave", function (event) {
-      console.log("ESTOY FUERA DEL TERRITORIO");
+    item.addEventListener("mouseleave", function (event) {
+      let x = event.layerY - maximumMeasure / 2;
+      let y = event.layerX - maximumMeasure / 2;
+
+      item.style.setProperty("--position-top-before", `${x}px`);
+      item.style.setProperty("--position-left-before", `${y}px`);
     });
   }
 });
-
-// $(".hover-animated .circle").on({
-
-//   mouseenter: function (e) {
-//     if ($(this).find(".ink").length === 0) {
-//       $(this).prepend("<span class='ink'></span>");
-//     }
-//     var ink = $(this).find(".ink");
-//     ink.removeClass("animate");
-//     if (!ink.height() && !ink.width()) {
-//       var d = Math.max($(this).outerWidth(), $(this).outerHeight());
-//       ink.css({ height: d, width: d });
-//     }
-//     var x = e.pageX - $(this).offset().left - ink.width() / 2;
-//     var y = e.pageY - $(this).offset().top - ink.height() / 2;
-//     ink.css({ top: y + "px", left: x + "px" }).addClass("ink-animate");
-//     $(".cursor-follower").addClass("hide");
-//   },
-
-//   mouseleave: function (e) {
-//     var ink = $(this).find(".ink");
-//     var x = e.pageX - $(this).offset().left - ink.width() / 2;
-//     var y = e.pageY - $(this).offset().top - ink.height() / 2;
-//     ink.css({ top: y + "px", left: x + "px" }).removeClass("ink-animate");
-//     $(".cursor-follower").removeClass("hide");
-//   },
-// });
