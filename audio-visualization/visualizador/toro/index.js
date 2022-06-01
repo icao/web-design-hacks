a6f459d1ca176263355bb1912dbdf2e2334217e4
César Aparicio <clark012389@hotmail.com>
2022-06-01 18:58:51 -0500

    Finalizando ejemplo

--- audio-visualization/visualizador/toro/index.js
+++ audio-visualization/visualizador/toro/index.js
@@ -21,14 +21,15 @@ archivoAudio.addEventListener("change", function () {
   analizador = audioContext.createAnalyser();
   fuenteAudio.connect(analizador);
   analizador.connect(audioContext.destination);
-  analizador.fftSize = 256; //  32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768
+  analizador.fftSize = 1024; //  32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768
 
   const longitudBuffer = analizador.frequencyBinCount;
   console.log(longitudBuffer);
   const datosArray = new Uint8Array(longitudBuffer);
 
   /** Configuracion visualizacion de las barras */
-  const anchoBarra = canvas.width / longitudBuffer;
+  // const anchoBarra = canvas.width / longitudBuffer;
+  const anchoBarra = 17;
   let altoBarra;
   let coordenadaX;
 
@@ -58,18 +59,15 @@ function dibujarVisualizador(
   datosArray
 ) {
   for (let i = 0; i < longitudBuffer; i++) {
-    altoBarra = datosArray[i]; //asiga el tamanio de bits como altura
-    const rojo = (i * altoBarra) / 20;
-    const verde = i * 4;
-    const azul = altoBarra / 2;
-    // canvasCtx.fillStyle = "#f36";
-    canvasCtx.fillStyle = `rgb(${rojo},${verde},${azul})`;
-    canvasCtx.fillRect(
-      coordenadaX,
-      canvas.height - altoBarra,
-      anchoBarra,
-      altoBarra
-    );
+    altoBarra = datosArray[i] * 1.5; //asiga el tamanio de bits como altura
+    canvasCtx.save();
+    canvasCtx.translate(canvas.width / 2, canvas.height / 2);
+    canvasCtx.rotate((i * (Math.PI * 10)) / longitudBuffer);
+
+    const hue = i * 0.4;
+    canvasCtx.fillStyle = `hsl(${hue}, 100%, 50%)`;
+    canvasCtx.fillRect(0, 0, anchoBarra, altoBarra);
     coordenadaX += anchoBarra;
+    canvasCtx.restore();
   }
 }
